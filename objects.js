f1ca7da58fb110cfabf82312f7bc288acda62095
Gonçalo Matos <matos98.ff@gmail.com>
2018-09-04 11:03:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { a: "Abc"};
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
